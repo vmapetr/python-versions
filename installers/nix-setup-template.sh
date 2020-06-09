be002215e178f262726096dddf8eb2e8fdea17a8
Maksim Petrov <v-mapetr@microsoft.com>
2020-06-09 14:37:00 +0300

    Test

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -52,6 +52,7 @@ chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR pyth
 echo "Upgrading PIP..."
 ./python -m ensurepip
 ./python -m pip install --ignore-installed pip
+./python -m pip install pysqlite3-binary
 
 echo "Create complete file"
 touch $PYTHON_TOOLCACHE_VERSION_PATH/x64.complete
